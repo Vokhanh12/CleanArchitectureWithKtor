880e9d0e6b2446bc87676d9968dafbd2b3288c45
khanh <khanhyou2018@gmail.com>
2023-11-15 14:06:22 +0700

    frist commit

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,16 @@
+package com.example
+
+import com.example.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+
+    configureSerialization()
+    configureDatabases()
+    configureMonitoring()
+    configureRouting()
+}
