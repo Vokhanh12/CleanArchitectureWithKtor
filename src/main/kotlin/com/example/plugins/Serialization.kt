233b6679a64b173d0c3c9746fc23b49445be3625
khanh <khanhyou2018@gmail.com>
2023-11-16 20:48:22 +0700

    add commit

--- src/main/kotlin/com/example/plugins/Serialization.kt
+++ src/main/kotlin/com/example/plugins/Serialization.kt
@@ -1,18 +1,28 @@
 package com.example.plugins
 
+import com.example.controllers.addOrderRoutes
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+        })
     }
     routing {
+
+        addOrderRoutes()
+
         get("/json/kotlinx-serialization") {
                 call.respond(mapOf("hello" to "world"))
             }
+
+
     }
 }
--- src/main/kotlin/com/example/repositories/inMemory/OrderRepository.kt
+++ src/main/kotlin/com/example/repositories/inMemory/OrderRepository.kt
@@ -2,5 +2,33 @@ package com.example.repositories.inMemory
 
 import com.example.entities.Order
 import com.example.repositories.interfaces.IOrderRepository
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 
-class OrderRepository : BaseRepository<Order>() , IOrderRepository
+object Orders : IntIdTable() {
+    val name = varchar("name", 255)
+}
+
+class OrderRepository : BaseRepository<Order>() , IOrderRepository {
+
+    override suspend fun getAll(): List<Order> {
+        return transaction {
+            Orders.selectAll().map { rowToOrder(it) }
+        }
+    }
+
+    override suspend fun findById(id: String): Order? {
+        return transaction {
+            Orders.select { Orders.id eq id.toInt() }.singleOrNull()?.let { rowToOrder(it) }
+        }
+    }
+
+    private fun rowToOrder(row: ResultRow): Order {
+        return Order(row[Orders.id].value.toString()).apply {
+            name = row[Orders.name]
+        }
+    }
+}
